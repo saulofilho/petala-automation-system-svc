3c318ee403136b1bfa45116053a8e9b8ae38a145
Saulo Filho <saulosilvaf@gmail.com>
2025-04-29 17:40:38 -0300

    rubocop

--- api/app/serializers/company_serializer.rb
+++ api/app/serializers/company_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CompanySerializer < Panko::Serializer
   attributes :id, :name, :cnpj, :cep, :street, :number, :city, :state, :user_id
 end
--- api/app/serializers/order_serializer.rb
+++ api/app/serializers/order_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class OrderSerializer < Panko::Serializer
   attributes :id, :name, :company_id
 end
